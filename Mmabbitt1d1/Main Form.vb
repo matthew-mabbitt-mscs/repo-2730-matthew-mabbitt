8ca3130f5ada177fd2d89e34465ca3b47666fe9b
matthew-mabbitt-mscs <31548864+matthew-mabbitt-mscs@users.noreply.github.com>
2017-09-13 13:12:58 -0500

    Add files via upload

---
+++ Mmabbitt1d1/Main Form.vb
@@ -0,0 +1,28 @@
+﻿' Name:         Bakery Project
+' Purpose:      Calculate the total number of items sold and total sales
+' Prgrammer:    Matt Mabbitt on 9/12/2017
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' prepare scren for the next sale
+        txtDonuts.Text = String.Empty
+        TxtMuffins.Text = String.Empty
+        lblTotalItems.Text = String.Empty
+        lblTotalSales.Text = String.Empty
+
+        ' send the focus to the donuts box
+        txtDonuts.Focus()
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        ' calculate number of items sold and total sales
+
+        lblTotalItems.Text = Val(txtDonuts.Text) + Val(TxtMuffins.Text)
+        lblTotalSales.Text = Val(lblTotalItems.Text) * 0.5
+        lblTotalSales.Text = Format(lblTotalSales.Text, "currency")
+    End Sub
+End Class
